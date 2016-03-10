6ff61588ade2ac1d8d8b5fa0a1ecf7707828a015
Ouvry Ludovic <ludovic.ouvry@aliceadsl.fr>
2016-03-10 11:27:19 +0100

    createUser

--- HashFile/src/CreateUser.java
+++ HashFile/src/CreateUser.java
@@ -1,3 +1,8 @@
+
+import javax.swing.JOptionPane;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,10 +14,21 @@
  * @author ouvryl
  */
 public class CreateUser {
+    public String login;
+    public String mdpCrypte; 
     public CreateUser(){
-        String login;
+       
         String mdp;
         
+        login = JOptionPane.showInputDialog(null, "Entrer votre nom", " Nouvelle utilisateur !", JOptionPane.QUESTION_MESSAGE);
+        // voir comment ne pas affihce le mdp
+        mdp= JOptionPane.showInputDialog(null, "Entrer votre mdp", "Nouvelle utilisateur !", JOptionPane.QUESTION_MESSAGE);
+        
+        StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+        String encryptedPassword = passwordEncryptor.encryptPassword(mdp);
+        mdpCrypte=encryptedPassword;
+        
+       
     }
     
     
