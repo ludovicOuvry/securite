565b97e2080a56bae5186cfcdf2e838be88ecdbb
Ouvry Ludovic <ludovic.ouvry@aliceadsl.fr>
2016-03-10 09:22:41 +0100

    source

---
+++ HashFile/src/CreateUser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Classe qui permet d'ajouter un nouvelle utilisateur
+ * @author ouvryl
+ */
+public class CreateUser {
+    public CreateUser(){
+        String login;
+        String mdp;
+        
+    }
+    
+    
+}
